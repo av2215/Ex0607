2fe4991efc912d14fffd2556c8ffc747f4ac31da
av2215 <av2215@bs.amalnet.k12.il>
2025-01-26 21:19:28 +0200

    commit2

--- app/src/main/java/com/example/ex0607/MainActivity.java
+++ app/src/main/java/com/example/ex0607/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ex0607;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +12,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+    int shaked1;
+  ImageButton btn;
+    Random ron;
+    ImageView iv;
+    String st;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.findViewById(R.id.btn);
+        iv.findViewById(R.id.iv);
+        ron=new Random();btn.findViewById(R.id.btn);
+        ron=new Random();
+    }
+
+    public void go(View view) {
     }
 }
